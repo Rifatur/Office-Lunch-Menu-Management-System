45312bb2b0bf74ee9aa1237912bd47dfb11a3fe4
Rifatur Rahman <35681257+Rifatur@users.noreply.github.com>
2024-06-28 22:48:32 +0600

    Added menu Create Command

--- OfficeLunchMenuSystem.Application/Features/Menu/Command/Create/CreateMenuCommandHandler.cs
+++ OfficeLunchMenuSystem.Application/Features/Menu/Command/Create/CreateMenuCommandHandler.cs
@@ -1,15 +1,26 @@
 ﻿using MediatR;
+using OfficeLunchMenuSystem.Infrastructure.Data;
 
 namespace OfficeLunchMenuSystem.Application.Features.Menu.Command.Create
 {
     public class CreateMenuCommandHandler : IRequestHandler<CreateMenuCommand, Guid>
     {
 
-
+        private readonly ApplicationDbContext _context;
+        public CreateMenuCommandHandler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
 
         public Task<Guid> Handle(CreateMenuCommand request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var menu = new LunchMenu
+            {
+                Name = request.Name,
+                Date = request.Date,
+                Description = request.Description
+            };
+            return menu.Id;
         }
     }
 }
--- OfficeLunchMenuSystem.Web/obj/Debug/net8.0/OfficeLunchMenuSystem.Web.AssemblyInfo.cs
+++ OfficeLunchMenuSystem.Web/obj/Debug/net8.0/OfficeLunchMenuSystem.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OfficeLunchMenuSystem.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ce662a75817f69728817ceaf5b202e61bc5f786")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20bac1e8dcfd0e599695dba5f9ba7600286b58ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("OfficeLunchMenuSystem.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OfficeLunchMenuSystem.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
